b1190582ff50b999484ef8ff966c743affbadcab
msh07 <msh07@soong>
2024-06-09 21:58:05 +0900

    2024.06.09 집에서 JSP & DB 연동 연습중.

---
+++ JSPatHome/src/main/java/fileupload/FileUtil.java
@@ -0,0 +1,63 @@
+package fileupload;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.Part;
+
+public class FileUtil {
+	//파일 업로드
+	
+	public static String uploadFile(HttpServletRequest req, String sDirectory) throws ServletException, IOException {
+		
+		Part part = req.getPart("attachedFile");
+		String partHeader = part.getHeader("content-disposition");
+		String[] phArr = partHeader.split("filename=");
+		String originalFileName = phArr[1].trim().replace("\"", "");
+		
+		if (!originalFileName.isEmpty()) {
+			part.write(sDirectory + File.separator + originalFileName);
+		}
+		
+		return originalFileName;
+	}
+	
+	public static String renameFile(String sDirectory, String fileName) {
+		String ext = fileName.substring(fileName.lastIndexOf("."));
+		String now = new SimpleDateFormat("yyyyMMdd_HmsS").format(new Date());
+		String newFileName = now + ext;
+		
+		File oldFile = new File(sDirectory + File.separator + fileName);
+		File newFile = new File(sDirectory + File.separator + newFileName);
+		oldFile.renameTo(newFile);
+		
+		return newFileName;
+	}
+	
+	//miltiple 속성 추가로 2개 이상의 파일 업로드
+	public static ArrayList<String> multipleFile(HttpServletRequest req, String sDirectory) throws ServletException, IOException {
+		
+		ArrayList<String> listFileName = new ArrayList<>();
+		Collection<Part> parts = req.getParts();
+		for (Part part : parts) {
+		
+			if(!part.getName().equals("attachedfile"))
+				continue;
+			
+			String partHeader = part.getHeader("content-disposition");
+			String[] phArr = partHeader.split("filename=");
+			String originalFileName = phArr[1].trim().replace("\"", "");
+			if (!originalFileName.isEmpty()) {
+				part.write(sDirectory+ File.separator +originalFileName);
+			}
+			listFileName.add(originalFileName);
+		}
+		return listFileName;
+	}
+}
